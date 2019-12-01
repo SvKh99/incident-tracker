20a7fb9ee54a73467e284a8b76f3039bd3e0f3b6
SvKh99 <kris.alex.sveta@gmail.com>
2019-12-01 21:17:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/i<name-of-app>'));
+app.use(express.static('./dist/NetcrackerApp'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname,'/dist/<name-of-app>/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/NetcrackerApp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
